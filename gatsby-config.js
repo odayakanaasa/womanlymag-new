ca3cbe786022ebd2d7aa76de07bef53b15841d6f
Rae Farine <rfarine1027@gmail.com>
2017-12-23 13:23:12 -0500

    add contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
+require('dotenv-safe').config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
   siteMetadata: {
-    title: `Womanly Mag`,
+    title: 'Womanly Mag',
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: `${process.env.CONTENTFUL_SPACE_ID}`,
+        accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+        host: !isProduction ? 'preview.contentful.com' : 'cdn.contentful.com',
+      },
+    },
+  ],
 };
